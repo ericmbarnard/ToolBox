bb65be01dca7b671e99b00c63061bc262471bc5b
ericmbarnard <eric.m.barnard@gmail.com>
2012-05-22 23:48:13 -0500

    Built out Domain Events properly

---
+++ CSharp/Architecture/Events/DomainEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Architecture.Events
+{
+    /// <summary>
+    /// Marker Class for Arguments that will be passed to Event Handling code
+    /// </summary>
+    public abstract class DomainEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Indicates that further event processing should not continue
+        /// </summary>
+        public bool Cancel { get; set; }
+    }
+}
--- CSharp/Architecture/Events/DomainEventHandler.cs
+++ CSharp/Architecture/Events/DomainEventHandler.cs
@@ -10,61 +10,58 @@ namespace Architecture.Events
     /// <summary>
     /// Defines a Handler for Domain Events
     /// </summary>
-    /// <typeparam name="T">The <see cref="IDomainEvent"/> Type to be Handled</typeparam>
-    public abstract class DomainEventHandler<T> where T : IDomainEvent
+    /// <typeparam name="T">The <see cref="DomainEventArgs"/> Type to be Handled</typeparam>
+    public abstract class DomainEventHandler<T> where T : DomainEventArgs
     {
-        /// <summary>
-        /// Override handler for Query Execution
-        /// </summary>
-        public Func<Query, object> QueryExecutor;
-
-        /// <summary>
-        /// Override handler for Command Execution
-        /// </summary>
-        public Func<Command, object> CommandExecutor;
-
-        /// <summary>
-        /// Utility Method for executing queries inside of a Command
-        /// </summary>
-        /// <typeparam name="T">The Query Result Type</typeparam>
-        /// <param name="qry">The Query to Execute</param>
-        /// <returns>The Query Result</returns>
-        public T Query<T>(Query<T> qry)
+        protected DomainEventHandler()
         {
-            if (QueryExecutor == null)
-            {
-                var executor = new QueryExecutor();
-                return executor.Query(qry);
-            }
-            else
-            {
-                return (T)QueryExecutor(qry);
-            }
-        }
-
-        /// <summary>
-        /// Utility Method for executing commands inside of a Domain Event Handler
-        /// </summary>
-        /// <typeparam name="TResult">The Command Result</typeparam>
-        /// <param name="cmd">The Command to execute</param>
-        /// <returns>The Result of the Command</returns>
-        public TResult ExecuteCommand<TResult>(Command<TResult> cmd)
-        {
-            if (CommandExecutor == null)
-            {
-                var executor = new CommandExecutor();
-                return executor.ExecuteCommand(cmd);
-            }
-            else
-            {
-                return (TResult)CommandExecutor(cmd);
-            }
+            QueryExecutor = new QueryExecutor();
+            CommandExecutor = new CommandExecutor();
+            Priority = 0;
         }
 
         /// <summary>
         /// The logic to be executed by the Handler
         /// </summary>
-        /// <param name="args">The <see cref="IDomainEvent"/> to be handled</param>
+        /// <param name="args">The <see cref="DomainEventArgs"/> to be handled</param>
         public abstract void Handle(T args);
+
+        /// <summary>
+        /// The priority order in which the handlers will be executed. 
+        /// Positive numbers increase Priority, Zero and Negatives decrease priority
+        /// </summary>
+        public int Priority { get; set; }
+
+        /// <summary>
+        /// The execution logic for Queries
+        /// </summary>
+        public QueryExecutor QueryExecutor { get; private set; }
+
+        /// <summary>
+        /// The execution logic for Commands
+        /// </summary>
+        public CommandExecutor CommandExecutor { get; private set; }
+
+        /// <summary>
+        /// Executes a <see cref="Query"/>
+        /// </summary>
+        /// <typeparam name="TResult">The typeof Result from the Query</typeparam>
+        /// <param name="qry">The Query to Execute</param>
+        /// <returns>A Query Result</returns>
+        protected TResult Query<TResult>(Query<TResult> qry)
+        {
+            return QueryExecutor.Query(qry);
+        }
+
+        /// <summary>
+        /// Executes a <see cref="Command"/>
+        /// </summary>
+        /// <typeparam name="TResult">The typeof Result from the Command</typeparam>
+        /// <param name="command">The Command to Execute</param>
+        /// <returns>A Command Result</returns>
+        protected TResult ExecuteCommand<TResult>(Command<TResult> command)
+        {
+            return CommandExecutor.ExecuteCommand(command);
+        }
     }
 }
